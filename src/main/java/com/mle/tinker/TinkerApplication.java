ebb8a7baf2c9bd40bb936c2ecdaad8844b16ac4a
markleighevans <markleighevans@sky.con>
2015-09-21 21:32:48 +0100

    First committ

--- src/main/java/com/mle/tinker/TinkerApplication.java
+++ src/main/java/com/mle/tinker/TinkerApplication.java
@@ -1,5 +1,8 @@
 package com.mle.tinker;
-
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 import com.mle.tinker.dao.PersonDAO;
 import com.mle.tinker.resources.PersonResource;
 import io.dropwizard.Application;
@@ -24,6 +27,20 @@ public class TinkerApplication extends Application<TinkerConfiguration> {
 
     @Override
     public void run(TinkerConfiguration configuration, Environment environment) throws ClassNotFoundException {
+        //**********************************************************************************************
+        // Enable CORS headers
+        //*************ADDED TO WORK AROUND No 'Access-Control-Allow-Origin' header issue************
+        //***********************************************************************************************
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        // *************************************************************
+        // *************************************************************
+
         final DBIFactory factory = new DBIFactory();
         final DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "h2");
 
